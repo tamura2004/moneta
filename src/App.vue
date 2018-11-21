deba1b1487f0754dfc9a2bf00c4607e470bc6436
tamura <tamura2004@gmail.com>
2018-11-21 22:13:45 +0900

    Fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template lang="pug">
-  v-app
-    v-toolbar(app)
-      v-toolbar-title.headline
-        span MONETA BANK
-      v-spacer
-      v-btn(flat)
-        span.mr-2 login:モネタ太郎様
-    v-content
-      router-view
+
+v-app
+  v-toolbar(app)
+    v-toolbar-title.headline
+      span MONETA BANK
+    v-spacer
+    v-btn(flat)
+      span.mr-2 login:モネタ太郎様
+  v-content
+    router-view
+
 </template>
 
 <script>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,22 +1,24 @@
 <template lang="pug">
-    v-container(grid-list-md text-xs-center)
-        v-layout(row wrap)
-            v-flex(md4 offset-md4)
-                router-link(to="/balance")
-                    v-btn.menu.my-4(round dark block large color="primary") 残高照会
-                v-btn.menu.my-4(round dark block large color="primary") 入出金明細
-                v-btn.menu.my-4(round dark block large color="primary") 振込
+  v-container(grid-list-md text-xs-center)
+    v-layout(row wrap)
+      v-flex(
+        xs6 offset-xs3
+        sm4 offset-sm4
+        md4 offset-md4
+        lg3 offset-lg5
+        xl3 offset-xl5
+      )
+        router-link(to="/balance")
+          v-btn.menu.my-4(round dark block large color="primary") 残高照会
+        v-btn.menu.my-4(round dark block large color="primary") 入出金明細
+        v-btn.menu.my-4(round dark block large color="primary") 振込
 </template>
 
-<script>
-export default {
-  name: 'Menu',
-  data() {
-    return {
-      //
-    };
-  },
-};
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component({})
+  export default class Menu extends Vue {}
 </script>
 
 <style lang="stylus">
