d023b5b8445fbd566ccf951dce7d16068aea069f
M.Tamura <tamura2004@gmail.com>
2018-11-22 19:31:47 +0900

    Add statement page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,19 @@
 <template lang="pug">
-  v-app
-    v-content
-      v-container(fluid fill-height)
-        v-layout(align-center justify-center)
-          v-flex(xs8 sm6 md4 lg3 xl2)
-            v-card.elevation-12
-              v-toolbar(dark color="primary")
-                v-toolbar-title ログイン
-              v-card-text
-                v-form
-                  v-text-field(prepend-icon="person" name="ID" label="ID" type="text")
-                  v-text-field(id="password" prepend-icon="lock" name="password" label="password" type="text")
-              v-card-actions
-                v-spacer
-                v-btn(color="primary" @click="menu") ログイン
+v-app
+  v-content
+    v-container(fluid fill-height)
+      v-layout(align-center justify-center)
+        v-flex(xs8 sm6 md4 lg3 xl2)
+          v-card.elevation-12
+            v-toolbar(dark color="primary")
+              v-toolbar-title ログイン
+            v-card-text
+              v-form
+                v-text-field(prepend-icon="person" name="ID" label="ID" type="text")
+                v-text-field(id="password" prepend-icon="lock" name="password" label="password" type="text")
+            v-card-actions
+              v-spacer
+              v-btn(color="primary" @click="menu") ログイン
 </template>
 
 <script lang="ts">
--- src/views/Statement.vue
+++ src/views/Statement.vue
@@ -1,15 +1,98 @@
 <template lang="pug">
-    v-container(grid-list-md text-xs-center)
+  v-app
+    v-toolbar(app)
+      v-toolbar-title.headline
+        v-btn(flat icon x-large @click="back")
+          v-icon arrow_back
+          span 戻る
+      v-spacer
+      v-btn(flat)
+        span.mr-2 login:モネタ太郎様
+    v-content
+      v-container(fluid)
         v-layout(row wrap)
-            v-flex(md4 offset-md4)
-                v-btn.menu.my-4(round dark block large color="primary") 入出金明細
+          v-flex(
+            xs10 offset-xs1
+            sm10 offset-sm1
+            md8 offset-md2
+            lg6 offset-lg3
+            xl6 offset-xl3
+          )
+            h2 入出金明細
+            v-data-table.elevation-8(:headers="headers" :items="statements")
+              template(slot="items" slot-scope="props")
+                td {{ props.item.date }}
+                td {{ props.item.type }}
+                td {{ props.item.amount }}
+                td {{ props.item.comment }}
+                td {{ props.item.total }}
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
-@Component({
-  components: {
-  },
-})
-export default class Statement extends Vue {}
+
+interface Header {
+  text: string;
+  value: string;
+}
+
+interface Cell {
+  date: string;
+  type: string;
+  amount: number;
+  comment: string;
+  total: number;
+}
+
+@Component
+export default class Statement extends Vue {
+  public headers: Header[] = [
+    {
+      text: '日付',
+      value: 'date',
+    },
+    {
+      text: '種別',
+      value: 'type',
+    },
+    {
+      text: '金額',
+      value: 'amount',
+    },
+    {
+      text: '適用',
+      value: 'comment',
+    },
+    {
+      text: '合計',
+      value: 'total',
+    },
+  ];
+  public statements: Cell[] = [
+    {
+      date: '2018/11/01',
+      type: '入',
+      amount: 3120,
+      comment: '',
+      total: 13120,
+    },
+    {
+      date: '2018/11/01',
+      type: '入',
+      amount: 312000,
+      comment: '給与',
+      total: 1312000,
+    },
+    {
+      date: '2018/11/01',
+      type: '出',
+      amount: 10220,
+      comment: 'ガス料金',
+      total: 1301780,
+    },
+  ];
+  private back($event: Event): void {
+    this.$router.push('/');
+  }
+}
 </script>
