0e53aaabace19ff58161051452df929513aaa346
M.Tamura <tamura2004@gmail.com>
2019-01-28 14:06:33 +0900

    deploy

---
+++ api/public/precache-manifest.a456ca19770fb54ce17654f025ecc724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "04a6c38f46cfc61b40e5",
+    "url": "/css/app.2f5c429a.css"
+  },
+  {
+    "revision": "04a6c38f46cfc61b40e5",
+    "url": "/js/app.6649d044.js"
+  },
+  {
+    "revision": "6496e9c367219935205c",
+    "url": "/css/chunk-vendors.eb055269.css"
+  },
+  {
+    "revision": "6496e9c367219935205c",
+    "url": "/js/chunk-vendors.71db3b54.js"
+  },
+  {
+    "revision": "dbe80c8a27ff317a113d01dcdd6f8ac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- api/public/service-worker.js
+++ api/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17361ec3ccaefc8135004a4127e6bfd1.js"
+  "/precache-manifest.a456ca19770fb54ce17654f025ecc724.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "moneta"});
