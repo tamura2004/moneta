81d9b9bb387ed2b4cfdd361a295394c212bf2c17
tamura <tamura2004@gmail.com>
2019-03-06 08:20:16 +0900

    Add vuetify count tool

---
+++ tools/count_vuetify_component.rb
@@ -0,0 +1,22 @@
+# ソースコードの中から、v-tile-titleなどvuetifyのコンポーネントを
+# 探して表示する
+
+require "pathname"
+require "kconv"
+require "set"
+
+$dic = Set.new
+
+Pathname("src").find do |path|
+  next unless path.file?
+
+  path.each_line do |line|
+    line = line.toutf8
+    line.chomp.split(/[\s\(]/).each do |word|
+      if word =~ /^v\-[\w\-]+$/
+        $dic.add(word)
+      end
+    end
+  end
+end
+p $dic.to_a.sort
