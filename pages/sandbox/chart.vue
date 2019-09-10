1d70c5a5d9f5649cfa1e8a8e8e27df5ed4c8f87e
M.Tamura <tamura2004@gmail.com>
2019-09-10 11:15:48 +0900

    Update chart from account total data

--- pages/sandbox/chart.vue
+++ pages/sandbox/chart.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <h1 class="display-2">グラフの例</h1>
-    <chart :data="data"></chart>
+    <chart :data="nodes"></chart>
     <a href="https://www.chartjs.org/docs/latest/">https://www.chartjs.org/docs/latest</a>
     <menu-button to="/sandbox">戻る</menu-button>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import chart from "~/components/pages/sandbox/chart";
 import menuButton from "~/components/ui/menu-button";
 
@@ -18,7 +19,7 @@ export default {
   },
   layout: "admin",
   data: () => ({
-    data: {
+    nodes: {
       labels: ["1月", "2月", "3月", "4月"],
       datasets: [
         {
@@ -28,5 +29,10 @@ export default {
       ]
     }
   }),
+  computed: mapGetters("accounts", ["collection"]),
+  created() {
+    this.nodes.labels = this.collection.map(a => a.name);
+    this.nodes.datasets[0].data = this.collection.map(a => a.total);
+  },
 }
 </script>
