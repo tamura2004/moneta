b07ba5e2c2c06e057e697cee9723d6527f6817b0
tamura <tamura2004@gmail.com>
2019-09-18 21:47:21 +0900

    Update test

--- test/pages/banks/index.test.js
+++ test/pages/banks/index.test.js
@@ -1,47 +1,14 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Component from "~/pages/admin/banks/index.vue";
-import Vuex from "vuex";
 
 describe("pages/admin/banks/index.vue", () => {
   test("is Vue instance", () => {
     const localVue = createLocalVue();
-    localVue.use(Vuex);
-    const store = new Vuex.Store({
-      modules: {
-        banks: {
-          namespaced: true,
-          getters: {
-            banks: state => [],
-          },
-          actions: {
-            remove: () => {},
-          },
-        },
-        nav: {
-          namespaced: true,
-          modules: {
-            namespaced: true,
-            edit: {
-              getters: {
-                edit: state => null,
-              },
-              actions: {
-                toggle: () => {},
-              },
-            },
-          },
-        },
-      },
-    });
     const wrapper = shallowMount(Component, {
-      store,
       localVue,
-      stubs: {
-        "v-card": true,
-        "v-divider": true,
-      },
+      stubs: ["v-card", "v-divider", "list-toolbar", "list-actions", "app-list"],
       mocks: {
-        $title: () => {},
+        $read: () => [],
       },
     });
     expect(wrapper.isVueInstance()).toBeTruthy();
